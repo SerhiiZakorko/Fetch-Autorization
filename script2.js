6005bd35fa2a30cdc90c67c9531ee8b862b8613f
SerhiiZakorko <129493695+SerhiiZakorko@users.noreply.github.com>
2023-09-05 21:01:21 +0200

    Added styles

--- script2.js
+++ script2.js
@@ -4,6 +4,7 @@ const name = document.querySelector(".name");
 const mail = document.querySelector(".mail");
 const goTo1 = document.querySelector(".button-log1");
 const login = document.querySelector(".login");
+const message = document.querySelector(".message");
 
 goTo1.addEventListener("click", () => {
   const nameValue = name.value;
@@ -13,23 +14,29 @@ goTo1.addEventListener("click", () => {
       return response.json();
     })
     .then((data) => {
-      data.length === 0 ? alert("Invalid data") : render();
+      data.length === 0 ? (message.innerText = "Invalid data") : render();
     });
 });
 
 function render() {
-  login.innerHTML = `<div><input type="text" class="company" placeholder="Your company is: "><button class="button-log2">Go To</button></div>`;
+  login.innerHTML = `<div class ="login2"><input type="text" class="company" placeholder="Your company is: "><button class="button-log2">Go To</button><p class="message"></p></div>`;
   const goTo2 = document.querySelector(".button-log2");
   const company = document.querySelector(".company");
 
   goTo2.addEventListener("click", () => {
+    const message = document.querySelector(".message");
     const companyValue = company.value;
     fetch(url + `?name=${companyValue}`)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        data.length === 0 ? alert("Invalid data") : alert("Success");
+        data.length === 0
+          ? (message.innerText = "Invalid data")
+          : login.innerHTML = `<p class="message">Success!!</p><button class="back">Go back</button>`;
+
+        const backBtn = document.querySelector(".back");
+        backBtn.addEventListener("click", () => location.reload());
       });
   });
 }
